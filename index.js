ad28a797d68fd641c753d567bc9ad3215cad7783
LaurelButler <laurelbutler126@gmail.com>
2019-05-21 16:14:56 -0400

    finished height drill

--- index.js
+++ index.js
@@ -59,19 +59,58 @@ function main() {
   - get max depth left recursively
   - get max depth right recursively
   */
-  function height(t, h = 0){
-    if(!t){
-        return h;
-    }
-    h++;
-    let left = height(t.left, h);
-    let right = height(t.right, h);
 
-    console.log(h);
+  // function height(t, h = 0){
+  //   if(!t){
+  //       return h;
+  //   }
+  //   h++;
+  //   let left = height(t.left, h);
+  //   let right = height(t.right, h);
+
+  //   console.log(h);
     
-  }
+  // }
 
-  height(bstNumbers);
+  // height(bstNumbers);
+
+  function height(t) {
+    //base case
+    if(!t) {
+      return 0;
+    }
+    let left = height(t.left);
+    let right = height(t.right);
+    if (left > right) {
+      return left +1;
+    } else {
+      return right +1;
+    }
+  }
+  //console.log(height(bstNumbers));
+
+  /* Write an algorithm to check whether an arbitrary binary tree is a binary search tree, assuming the tree does not contain duplicates.
+  -check to see if its a tree
+  */
+  // function binTreeSearch(t) {
+  //   //base case
+  //   if(!t) {
+  //     return true;
+  //   }
+  //   let left = height(t.left);
+  //   let right = height(t.right);
+  //   if(left !== 0 && t.value >)
+  // }
+
+  /* Write an algorithm to find the 3rd largest node in a binary search tree. */
+  function thirdLargestNode (t) {
+    if(!t.right) {
+      return t;
+    }
+    let right = height(t.right);
+    return thirdLargestNode(right);
+  }
+  console.log(thirdLargestNode(bstNumbers));
 }
 
 main()
