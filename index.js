e4646a6bed42e6e7b557d48b9c0435cdc4bb2352
Joel Corey <joelcorey@fastmail.com>
2019-05-21 11:23:36 -0700

    draw a BST answered in commments

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+/*
+- Draw a BST
+Given the data [3, 1, 4, 6, 9, 2, 5, 7]
+https://imgur.com/a/cYY9wfl
+- From this BST, delete the root and re-draw it
+https://imgur.com/a/6wFstXJ
+
+- Draw a BST
+Given the data [E, A, S, Y, Q, U, E, S, T, I, O, N]
+https://imgur.com/a/rDF3hbZ
+- From this BST, delete the root and re-draw it
+https://imgur.com/a/bCPFeDd
+*/
+
 const binarySearchTree = require('./BinarySearchTree.js');
 
 function main() {
-  bst = new binarySearchTree;
+  //bst = new binarySearchTree;
+
+  var arr = [ 0, 1, 2, 3, 4, 5, 6];
+  var middle = arr[Math.floor(arr.length / 2)];
+  console.log(middle)
 }
+
+main()
